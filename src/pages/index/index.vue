bedfe136eba06e9596061c5f832796ec25502e84
123 <a123@123deMacBook-Air.local>
2018-09-17 18:59:11 +0800

    记不清楚改了啥子了。。。

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home container">
-    <button class="tr">
+    <button disabled="true" class="tr">
       <div class="th">Type</div>
       <div class="th f0">Addr</div>
       <div class="th f0">Link</div>
@@ -8,7 +8,7 @@
       <div class="th">Value</div>
     </button>
     <ul class="tb">
-      <button  v-for="(item, index) in list" :key="index" class="tr">
+      <button plain="true" size="mini" v-for="(item, index) in list" :key="index" class="tr">
         <div class="td"><img class="icon" :src="iconMap[item.SensorName]"/>{{item.SensorName}}</div>
         <div class="td f0">{{item.SensorAddr}}</div>
         <div class="td f0"><img class="icon" :src="iconMap[item.LinkState]"/></div>
@@ -20,7 +20,6 @@
 </template>
 
 <script>
-
   const iconMap = {
     烟雾: '/static/images/common/smog.png',
     开停: '/static/images/common/switch.png',
@@ -30,7 +29,6 @@
     激光甲烷: '/static/images/common/line.png',
     二氧化碳: '/static/images/common/co2.png',
     风速: '/static/images/common/speed.png',
-    等待连接: '/static/images/common/wait.png',
     读卡器: '/static/images/common/card.png',
     甲烷低浓度: '/static/images/common/ch4.png',
     广播终端: '/static/images/common/boardcast.png',
@@ -40,14 +38,13 @@
     语音风门: '/static/images/common/fengmen.png',
     风筒: '/static/images/common/fengtong.png',
     电源: '/static/images/common/power.png',
-
     C1: '/static/images/common/index1.png',
     C2: '/static/images/common/index2.png',
     C3: '/static/images/common/index3.png',
     C4: '/static/images/common/index4.png',
-
     连接正常: '/static/images/common/link.png',
     连接断开: '/static/images/common/linkbreak.png',
+    等待连接: '/static/images/common/waitlink.png',
   };
 
   export default {
@@ -57,7 +54,6 @@
         iconMap,
       };
     },
-
     methods: {
       clickHandle(msg, ev) {
         console.log('clickHandle:', msg, ev);
@@ -90,15 +86,22 @@
   .home {
     padding: 20rpx;
   }
+.tb{
+    .button-hover {
+  background-color: rgba(0, 255, 242, 0.329);
+}
+  .other-button-hover {
+  background-color: blue;
+}
+}
   .tr {
     display: flex;
-    height: 70rpx;
     .th, .td {
       position: relative;
       flex: 2;
-      //text-align: center;
-      font-size: 75%;
-      padding: 0rpx 0;
+      text-align: center;
+      font-size: 90%;
+      padding: 8rpx 0;
       padding-left: 5rpx;
       &:after {
         .hairline();
